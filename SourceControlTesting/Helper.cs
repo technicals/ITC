f01e4c72386b72fe167061629b9ef6bf7e45a458
ahmedkashif <kashifahmed.bse.mg@gmail.com>
2015-02-03 12:31:46 +0500

    Helper class added by kashifahmeds.

---
+++ SourceControlTesting/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SourceControlTesting
+{
+    // purpose of this class is to add helper functions.
+    public class Helper
+    {
+
+    }
+}
